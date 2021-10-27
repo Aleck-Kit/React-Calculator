1afb3f04588b1b59d0b43499d4d2c9d62091305c
Aleck-Kit <aleck.kit@protonmail.com>
2021-10-27 17:45:59 +0200

    additional changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,18 @@
+import React from 'react';
 import './Button.css';
 
 
 const isOperator = val => {
-    return  !isNaN(val) || val === "." || val === "=";
-    
+    return !isNaN(val) || val === "." || val === "=";
 };
 
-
-const Button = props => (
-    <div 
-        className={`buttonwrapper ${
-            isOperator(props.children) ? null : "operator"
-        }`}
-        onClick={() => props.handleClick(props.children)}
-
+export const Button = props => (
+    <div
+    className={`button-wrapper ${
+        isOperator(props.children) ? null : "operator"
+    }`}
+    onClick={() => props.handleClick(props.children)}
     >
-        {props.children}
+    {props.children}
     </div>
-)
-export default Button;
+);
