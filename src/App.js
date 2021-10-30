ac12fe5c3b5021f0629edb2b65616687bdf7e318
Aleck-Kit <aleck.kit@protonmail.com>
2021-10-30 07:50:10 +0200

    changes to background, header and buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Button } from './components/Button';
 import {Input} from './components/Input';
 import {ClearButton} from './components/ClearButton';
+import {Header} from './components/Header';
 import * as math from 'mathjs';
 
 
@@ -24,41 +25,45 @@ class App extends Component {
 
     render() { 
         return ( 
-            <div className="app">
-                
-                <div className="calc-wrapper">
-                    <Input input={this.state.input} />
-                    <div className="row">
-                        <Button handleClick={this.addToInput}>7</Button>
-                        <Button handleClick={this.addToInput}>8</Button>
-                        <Button handleClick={this.addToInput}>9</Button>
-                        <Button handleClick={this.addToInput}>/</Button>
-                    </div>
-                    <div className="row">
-                        <Button handleClick={this.addToInput}>4</Button>
-                        <Button handleClick={this.addToInput}>5</Button>
-                        <Button handleClick={this.addToInput}>6</Button>
-                        <Button handleClick={this.addToInput}>*</Button>
-                    </div>
-                    <div className="row">
-                        <Button handleClick={this.addToInput}>1</Button>
-                        <Button handleClick={this.addToInput}>2</Button>
-                        <Button handleClick={this.addToInput}>3</Button>
-                        <Button handleClick={this.addToInput}>+</Button>
-                    </div>
-                    <div className="row">
-                        <Button handleClick={this.addToInput}>.</Button>
-                        <Button handleClick={this.addToInput}>0</Button>
-                        <Button handleClick={() => this.handleEqual()}>=</Button>
-                        <Button handleClick={this.addToInput}>-</Button>
-                    </div>
-                    <div className="row">
-                        <ClearButton handleClear={() => this.setState({ input: "" })}>
-                            Clear
-                        </ClearButton>
+            <section>
+                <Header/>
+                <div className="app">
+                    
+                    <div className="calc-wrapper">
+                        <Input input={this.state.input} />
+                        <div className="row">
+                            <Button handleClick={this.addToInput}>7</Button>
+                            <Button handleClick={this.addToInput}>8</Button>
+                            <Button handleClick={this.addToInput}>9</Button>
+                            <Button handleClick={this.addToInput}>/</Button>
+                        </div>
+                        <div className="row">
+                            <Button handleClick={this.addToInput}>4</Button>
+                            <Button handleClick={this.addToInput}>5</Button>
+                            <Button handleClick={this.addToInput}>6</Button>
+                            <Button handleClick={this.addToInput}>*</Button>
+                        </div>
+                        <div className="row">
+                            <Button handleClick={this.addToInput}>1</Button>
+                            <Button handleClick={this.addToInput}>2</Button>
+                            <Button handleClick={this.addToInput}>3</Button>
+                            <Button handleClick={this.addToInput}>+</Button>
+                        </div>
+                        <div className="row">
+                            <Button handleClick={this.addToInput}>.</Button>
+                            <Button handleClick={this.addToInput}>0</Button>
+                            <Button handleClick={() => this.handleEqual()}>=</Button>
+                            <Button handleClick={this.addToInput}>-</Button>
+                        </div>
+                        <div className="row">
+                            <ClearButton handleClear={() => this.setState({ input: "" })}>
+                                Clear
+                            </ClearButton>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
+            
         );
     }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = () => {
+    return ( 
+        <header className="title">
+            <h1>React Calculator</h1>
+        </header>
+    );
+}
+
